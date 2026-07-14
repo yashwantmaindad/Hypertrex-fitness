a89648c6cacfec9ce2d1ff4288e553744c7f98f9
Pushpak-Pandore <pushpak.pandore@gmail.com>
2026-07-14 17:18:11 +0530

    Update Instagram link to clean profile URL (no tracking params)

--- src/config/site.ts
+++ src/config/site.ts
@@ -12,7 +12,7 @@ export const siteConfig = {
     googleMaps: "",
   },
   socials: {
-    instagram: "https://www.instagram.com/yashwan7_m?igsh=MXZyNmJ1b3NnenptYQ==",
+    instagram: "https://www.instagram.com/yashwan7_m",
     facebook: "", 
     youtube: "",
     linkedin: "",
